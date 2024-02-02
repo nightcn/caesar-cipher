aa49fc28a9009b61a11d25951fa84e34b5a28f3f
TBS-PC <tsahibspele@gmail.com>
2024-02-02 15:43:15 +0200

    END

--- substrings/sstrings.rb
+++ substrings/sstrings.rb
@@ -1,1 +1,16 @@
-placeholder
+def substrings(str, dictionary)
+    for i in 0...str.split(" ").length do
+        sstrings = dictionary.reduce(sstrings || Hash.new(0)) do |ssObj, word|
+            ssObj[word] += 1 if /#{word}/i.match?(str.split(" ")[i])
+            ssObj
+        end
+    end
+    sstrings
+end
+
+
+p substrings("below up low", ["below", "low"])
+
+dic = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+p substrings("Howdy partner, sit down! How's it going?", dic)
