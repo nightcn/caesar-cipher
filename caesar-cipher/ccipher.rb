f4bfc0cd39b6bf5e9b9f04b487bc14ab06724807
Shir <nightcn@gmail.com>
2024-02-02 14:30:01 +0200

    Update and rename ccipher.irb to ccipher.rb

---
+++ caesar-cipher/ccipher.rb
@@ -0,0 +1,19 @@
+def caesar_cipher(word, shift)
+    az_upcase = ('A'..'Z').to_a
+    az_lowcase = ('a'..'z').to_a
+    
+    encrypt = word.split("").map do |letter|
+        if az_upcase.include?(letter)
+            newIdx = (az_upcase.index(letter) + shift) % 26
+            az_upcase[newIdx]
+        elsif az_lowcase.include?(letter)
+            newIdx = (az_lowcase.index(letter) + shift) % 26 
+            az_lowcase[newIdx]
+        else
+            letter
+        end
+    end
+    encrypt.join("")
+end
+
+puts caesar_cipher('What a string!', 5)
